1b34e19263150e9bf25f153d3e01ab81ad37e7dc
Uriel Werneck <uriel_werneck@hotmail.com>
2025-07-20 15:14:07 -0300

    feat: add table relationship

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_restful import Api, Resource, abort
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields, ValidationError
 from marshmallow.validate import Length
@@ -13,17 +15,32 @@ api = Api(app)
 db = SQLAlchemy(app)
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    description = Column(String(200), nullable=False)
+    completed = Column(Boolean, default=False)
+
+class User(db.Model):
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), nullable=False)
+    password = Column(String(20), nullable=False)
+    tasks = relationship('Task', backref='user')
 
 class TaskSchema(Schema):
     id = fields.Int(dump_only=True)
+    # user_id = fields.Int(load_only=True)
     description = fields.Str(required=True, validate=Length(min=1))
     completed = fields.Bool(load_default=False)
 
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True, validate=Length(min=5, max=25))
+    password = fields.Str(required=True, validate=Length(min=5, max=25))
+
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 def get_task_or_404(task_id):
     task = Task.query.get(task_id)
@@ -72,9 +89,41 @@ class TodoItem(Resource):
         db.session.delete(task)
         db.session.commit()
         return '', 204
+
+class UsersResource(Resource):
+    def get(self):
+        users = User.query.all()
+        users_schema.dump(users), 200
     
+    def post(self):
+        try:
+            data = user_schema.load(request.get_json())
+            print(data)
+        except ValidationError as error:
+            return error.messages, 400
+        print('registering new user')
+
+class UserResource(Resource):
+    def get(self, user_id):
+        print(f'getting user {user_id}')
+    
+    def delete(self, user_id):
+        print(f'deleting user with id: {user_id}')
+
+class LoginUser(Resource):
+    def post(self):
+        return {'message': 'user logged'}
+
+class RegisterUser(Resource):
+    def post(self):
+        return {'message': 'user registered'}
+
 api.add_resource(TodoItem, '/api/tasks/<int:task_id>')
 api.add_resource(TodoList, '/api/tasks')
+api.add_resource(UserResource, '/api/users/<int:user_id>')
+api.add_resource(UsersResource, '/api/users')
+api.add_resource(LoginUser, '/api/login')
+api.add_resource(RegisterUser, '/api/register')
 
 if __name__ == '__main__':
     with app.app_context():
