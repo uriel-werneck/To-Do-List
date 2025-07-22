515cacd083831862f1a9677640f31adbcf023b6d
Uriel Werneck <uriel_werneck@hotmail.com>
2025-07-22 19:47:10 -0300

    feat: redirect user if JWT token is present

--- frontend/login.js
+++ frontend/login.js
@@ -1,3 +1,16 @@
+/* 
+Use <div> or alert() to show errors
+Disable buttons, show spinners, or change button text during an ongoing request
+to prevent multiple clicks and inform the user.
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        window.location.href = 'index.html';
+    }
+});
+
 document.getElementById('login-btn').addEventListener('click', async () => { // is this really async?
     const username = document.getElementById('login-input').value.trim();
     const password = document.getElementById('pwd-input').value;
@@ -12,16 +25,12 @@ document.getElementById('login-btn').addEventListener('click', async () => { // 
         const data = await response.json();
         
         if (response.ok) {
-            console.log('Login successful:', data.message);
             // store token and redirect to the main page
             localStorage.setItem('access_token', data.token);
             window.location.href = 'index.html';
         } else {
             throw new Error(`HTTP error! Status: ${response.status}`)
         }
-
-        console.log(data)
-
     } catch (err) {
         console.error(err);
     }
