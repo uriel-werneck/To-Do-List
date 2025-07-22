f96d832fddb85f61f8581834396e1c691958b035
Uriel Werneck <uriel_werneck@hotmail.com>
2025-07-22 00:45:27 -0300

    feat: store token in local storage

--- frontend/login.js
+++ frontend/login.js
@@ -1,19 +1,25 @@
 document.getElementById('login-btn').addEventListener('click', async () => { // is this really async?
-    const username = document.getElementById('login-input');
-    const password = document.getElementById('pwd-input');
+    const username = document.getElementById('login-input').value.trim();
+    const password = document.getElementById('pwd-input').value;
     
     try {
         const response = await fetch('http://127.0.0.1:5000/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: username.value, password: password.value })
+            body: JSON.stringify({ username: username, password: password })
         });
         
-        if (!response.ok) {
+        const data = await response.json();
+        
+        if (response.ok) {
+            console.log('Login successful:', data.message);
+            // store token and redirect to the main page
+            localStorage.setItem('access_token', data.token);
+            window.location.href = 'index.html';
+        } else {
             throw new Error(`HTTP error! Status: ${response.status}`)
         }
 
-        const data = await response.json();
         console.log(data)
 
     } catch (err) {
