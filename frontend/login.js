7777d58d49c24f62da9464cc552034c805b6d504
Uriel Werneck <uriel_werneck@hotmail.com>
2025-08-17 16:45:06 -0300

    refactor(login.js): extract user info from login form

--- frontend/login.js
+++ frontend/login.js
@@ -4,14 +4,9 @@ Disable buttons, show spinners, or change button text during an ongoing request
 to prevent multiple clicks and inform the user.
 */
 
-document.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-        window.location.href = 'index.html';
-    }
-});
+document.getElementById('login-form').addEventListener('submit', async (event) => {
+    event.preventDefault(); // prevent form from submitting the default way
 
-document.getElementById('login-btn').addEventListener('click', async () => { // is this really async?
     const username = document.getElementById('login-input').value.trim();
     const password = document.getElementById('pwd-input').value;
     
@@ -19,17 +14,16 @@ document.getElementById('login-btn').addEventListener('click', async () => { // 
         const response = await fetch('http://127.0.0.1:5000/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: username, password: password })
+            body: JSON.stringify({ username, password })
         });
         
         const data = await response.json();
         
         if (response.ok) {
-            // store token and redirect to the main page
             localStorage.setItem('access_token', data.token);
             window.location.href = 'index.html';
         } else {
-            throw new Error(`HTTP error! Status: ${response.status}`)
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
     } catch (err) {
         console.error(err);
